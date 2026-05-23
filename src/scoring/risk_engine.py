2394cc3f4f203ce401a075f7d2bbd0284801b701
Charles Bulabula <charlesbulabula@users.noreply.github.com>
2026-05-23 15:05:01 +0000

    feat: add check for CloudFront distributions without WAF association
    Closes #2

--- src/scoring/risk_engine.py
+++ src/scoring/risk_engine.py
@@ -229,4 +229,4 @@ def ensure_score_table(dynamodb_resource: Any, table_name: str, region: str = "u
         waiter.wait(TableName=table_name)
         logger.info("DynamoDB table %s created", table_name)
 
-# _r 20260522160002-a3bfa79f
+# _r 20260523150501-67080d47
