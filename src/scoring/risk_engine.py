b18bbfedb24bc74241741e06d8ae14484e3bf2ca
Charles Bulabula <charlesbulabula@users.noreply.github.com>
2026-05-27 15:32:14 +0000

    fix: prevent suppressed findings from re-surfacing after rule update

--- src/scoring/risk_engine.py
+++ src/scoring/risk_engine.py
@@ -229,4 +229,4 @@ def ensure_score_table(dynamodb_resource: Any, table_name: str, region: str = "u
         waiter.wait(TableName=table_name)
         logger.info("DynamoDB table %s created", table_name)
 
-# _r 20260527133611-991f2f4a
+# _r 20260527153214-b3e5c3e0
